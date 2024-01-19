92252de8d2c3caf583cf704985191723fdbdba21
acapu <asyrafmus27@gmail.com>
2024-01-19 23:19:04 +0800

    Commit heh

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
-import os
+import os, pickle
+from fastbook import load_learner
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "static/upload"
+app.secret_key = "very-secret-key"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+def index():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('download_file', name=filename))
-    return render_template("index.html");
+        upload_file()
+    # test.predict(r"app\static\upload\L7160037.JPG")
+    return render_template("index.html")
+
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.root_path,
+                  app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+
 
 def allowed_file(filename):
     return '.' in filename and \
-            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS   
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
